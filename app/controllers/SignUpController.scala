15daa3f8041966dc753fde6d2af8ec6484b29ee1
sandeepmaity09 <sandeepmaity09@gmail.com>
2017-03-07 07:28:41 +0530

    SignUpController resolved

--- app/controllers/SignUpController.scala
+++ app/controllers/SignUpController.scala
@@ -1,11 +1,30 @@
 package controllers
 
+import Models.PersonDetails
+import models.PersonDetailsMapping
 import play.api.mvc._
+import services.PersonService
 
 class SignUpController extends Controller {
 
   def signUp = Action {
     Ok(views.html.signuppage())
+  }
+
+  def addPersonDetails = Action {implicit request =>
+    PersonDetailsMapping.personDetailsForm.bindFromRequest.fold(
+      formWithErrors => {
+        Redirect(routes.SignUpController.signUp()).flashing(
+          "error" -> "Something went wrong"
+        )
+      },
+      personData => {
+        PersonService.addPerson(personData)
+        Redirect(routes.ProfileController.profile()).withSession(
+          "username" -> personData.username
+        )
+      }
+    )
   }
 
 }
